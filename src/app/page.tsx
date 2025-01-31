9429e957a61ba139aa220b6c8e3e7cb74ea00c4a
Joey Liu <qizhi.liu@joinhandshake.com>
2025-01-31 14:52:45 -0800

    fix some ESLint issues preventing deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 import { useState, useEffect } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
