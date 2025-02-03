9d44829ef5cafa799533750cbe06ed42bfd3ea85
Joey Liu <qizhi.liu@joinhandshake.com>
2025-02-03 15:22:12 -0800

    no unused variables

--- src/app/next.config.ts
+++ src/app/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
-
 const nextConfig: NextConfig = {
   // Remove basePath and assetPrefix since we're using the root domain
   output: 'export',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 // import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
